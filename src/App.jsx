a8983c87cc89ebb0399901399861e303ff8366a1
Louis Peter <louisclarencepeters@gmail.com>
2023-11-16 16:39:46 +0100

    added the initial style for the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/header/NavBar.jsx'
+import Hero from './components/main/Hero.jsx'
 
 function App() {
 
   return (
-    <>
-      <Header />
+    < >
+      <div className='main-container'>
+        <Header />
+        <Hero />
+      </div>
     </>
   )
 }
---
+++ src/components/main/Hero.jsx
@@ -0,0 +1,19 @@
+import './Hero.scss'
+
+
+function Hero() {
+    return (
+        <div className='hero-container'>
+
+            <div className='name-motto'>
+                <h1>Destination Paradise</h1>
+                <h3>Explore the beauty of Zanzibar</h3>
+            </div>
+
+            <p>Welcome to Destination Paradise, your gateway to the enchanting Zanzibar Island! Imagine a place where each day is an adventure, and every horizon promises new discoveries.</p>
+
+        </div>
+    )
+}
+
+export default Hero
