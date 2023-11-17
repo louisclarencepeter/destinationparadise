e4738098fcee4c74284572fefe38850933188fdc
Louis Peter <louisclarencepeters@gmail.com>
2023-11-17 07:09:57 +0100

    added a component called excursions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './App.css'
 import Header from './components/header/NavBar.jsx'
 import Hero from './components/main/Hero.jsx'
+import Excursions from './components/main/Excursions.jsx';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <div className='main-container'>
         <Header />
         <Hero />
+        <Excursions />
       </div>
     </>
   )
---
+++ src/components/main/Excursions.jsx
@@ -0,0 +1,24 @@
+function Excursions() {
+    return (
+        <div className="excursions-container">
+            <h3>Roaming Retreats</h3>
+
+            <div className="excursions">
+
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Excursions
