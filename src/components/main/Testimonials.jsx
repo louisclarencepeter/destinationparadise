0de01aa76476a8e5769906a5c897bbfdbc897b44
Louis Peter <louisclarencepeters@gmail.com>
2023-11-21 11:24:45 +0100

    added an empty array

--- src/components/main/Testimonials.jsx
+++ src/components/main/Testimonials.jsx
@@ -4,6 +4,10 @@ import './Testimonials.scss';
 
 const Testimonials = () => {
 
+  const reviews = [
+    
+  ];
+
   return (
     <>
     <div className='testimonials'>
@@ -11,10 +15,13 @@ const Testimonials = () => {
       <Slide>
         <div className="each-slide-effect">
           <div >
+            <div> <img src="" alt="" /></div>
+            <img src="" alt="" />
             <span>&ldquo;Si !!! Mangroves trip is amazing !!! Y no solo lo que disfrutas navegando en un dhow!! You can also have a walk on the mangroves and enjoy the best sunset ever. Thanks to Louis and the team and congrats!&quot;</span>
           </div>
         </div>
         <div className="each-slide-effect">
+
           <div >
             <span>&ldquo;This trip was amazing our tour guide was knowledgeable, upbeat and friendly .. the dowe experience was amazing and the lagoon was a site to see. I was afraid to snorkel but my tour guide got in with me and made me feel safe. The food they provided was very delicious I did not expect a buffet style meal on the beach ! This is what added to the experience! Will definitely recommend.&quot;</span>
           </div>
