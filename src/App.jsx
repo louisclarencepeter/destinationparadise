835fddaf72480feba6f1b1d0cd7032b481bebae2
Louis Peter <louisclarencepeters@gmail.com>
2023-11-21 13:51:48 +0100

    removed unneed styles in the testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.css'
 import Header from './components/header/NavBar.jsx'
-import Hero from './components/main/Hero.jsx'
+import Hero from './components/main/Hero.jsx';
 import Excursions from './components/main/Excursions.jsx';
 import Gallery from './components/main/Gallery.jsx';
 import Testimonials from './components/main/Testimonials.jsx';
--- src/components/main/Testimonials.jsx
+++ src/components/main/Testimonials.jsx
@@ -4,34 +4,31 @@ import './Testimonials.scss';
 
 const Testimonials = () => {
 
-  const reviews = [
-    
-  ];
-
   return (
     <>
-    <div className='testimonials'>
-      <h3>Testimonials</h3>
-      <Slide>
-        <div className="each-slide-effect">
-          <div >
-            <div> <img src="" alt="" /></div>
-            <img src="" alt="" />
-            <span>&ldquo;Si !!! Mangroves trip is amazing !!! Y no solo lo que disfrutas navegando en un dhow!! You can also have a walk on the mangroves and enjoy the best sunset ever. Thanks to Louis and the team and congrats!&quot;</span>
+      <div className='testimonials'>
+        <h2>Testimonials</h2>
+        <Slide>
+          <div className="">
+            <div >
+              <div> <img src="" alt="" />
+              </div>
+              <img src="" alt="" />
+              <span>&ldquo;Si !!! Mangroves trip is amazing !!! Y no solo lo que disfrutas navegando en un dhow!! You can also have a walk on the mangroves and enjoy the best sunset ever. Thanks to Louis and the team and congrats!&quot;</span>
+            </div>
           </div>
-        </div>
-        <div className="each-slide-effect">
+          <div className="">
 
-          <div >
-            <span>&ldquo;This trip was amazing our tour guide was knowledgeable, upbeat and friendly .. the dowe experience was amazing and the lagoon was a site to see. I was afraid to snorkel but my tour guide got in with me and made me feel safe. The food they provided was very delicious I did not expect a buffet style meal on the beach ! This is what added to the experience! Will definitely recommend.&quot;</span>
+            <div >
+              <span>&ldquo;This trip was amazing our tour guide was knowledgeable, upbeat and friendly .. the dowe experience was amazing and the lagoon was a site to see. I was afraid to snorkel but my tour guide got in with me and made me feel safe. The food they provided was very delicious I did not expect a buffet style meal on the beach ! This is what added to the experience! Will definitely recommend.&quot;</span>
+            </div>
           </div>
-        </div>
-        <div className="each-slide-effect">
-          <div >
-            <span>&ldquo;We had a great time and really enjoyed smelling all the spices and learning about their uses, beyond cooking. Louis was very easy to communicate with and actually went above and beyond to help us book and brainstorm to resolve a couple of challenges that we had.&quot;</span>
+          <div className="">
+            <div >
+              <span>&ldquo;We had a great time and really enjoyed smelling all the spices and learning about their uses, beyond cooking. Louis was very easy to communicate with and actually went above and beyond to help us book and brainstorm to resolve a couple of challenges that we had.&quot;</span>
+            </div>
           </div>
-        </div>
-      </Slide>
+        </Slide>
       </div>
     </>
   );
