eb30bdc9a4a190f80d1cb06892db44130cb0f947
Louis Peter <louisclarencepeters@gmail.com>
2023-12-07 13:25:59 +0100

    refactored the excursion component and changed the image

--- src/components/main/Excursions.jsx
+++ src/components/main/Excursions.jsx
@@ -1,128 +1,74 @@
-import './Excursions.scss'
+import './Excursions.scss';
 import img from '../../assets/images/stone-town.jpeg';
 import img2 from '../../assets/images/safari-blue.jpg';
 import img3 from '../../assets/images/spice-tour.jpg';
 
-function Excursions() {
+const Excursions = () => {
+    const trips = [
+        {
+            title: 'Stone Town Heritage Walk',
+            text: 'Embark on a journey through the timeless Stone Town, a place where history resonates in every alley.',
+            link: '/learn-more',
+            discount: '10%',
+            image: img
+        },
+        {
+            title: 'Dhow & Snorkeling Safari Blue',
+            text: 'Experience the authentic and unrivaled Safari Blue - a full-day excursion aboard traditional, locally-crafted sailing dhows.',
+            link: '/learn-more',
+            discount: '10%',
+            image: img2
+        },
+        {
+            title: 'Zanzibar Spice & Culture Tour',
+            text: 'Embark on a half-day journey through Central Zanzibar, exploring the rich history shaped by cloves, nutmeg, cinnamon, and pepper.',
+            link: '/learn-more',
+            discount: '10%',
+            image: img3
+        }
+    ];
+
     return (
         <div className='card'>
             <h2>Roaming Retreats</h2>
-            <div className='trips'>
-                <div className="card-hover">
-                    <div className="card-hover__content">
-                        <h4 className="card-hover__title">
-                            Stone Town <span>Heritage</span> Walk
-                        </h4>
-                        <p className="card-hover__text">
-                            Embark on a journey through the timeless Stone Town, a place where history resonates in every alley.
-                        </p>
-                        <a href="/learn-more" className="card-hover__link" aria-label="Learn more about Stone Town Heritage Walk">
-                            <span>Learn More</span>
-                            <svg
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                aria-label="External Link"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                                />
-                            </svg>
-                        </a>
+            {trips.map((trip, index) => (
+                <div className='trips' key={index}>
+                    <div className='card-hover'>
+                        <div className='card-hover__content'>
+                            <h4 className='card-hover__title'>
+                                {trip.title}
+                            </h4>
+                            <p className='card-hover__text'>
+                                {trip.text}
+                            </p>
+                            <a href={trip.link} className='card-hover__link' aria-label={`Learn more about ${trip.title}`}>
+                                <span>Learn More</span>
+                                <svg
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    strokeWidth='1.5'
+                                    stroke='currentColor'
+                                    aria-label='External Link'
+                                >
+                                    <path
+                                        strokeLinecap='round'
+                                        strokeLinejoin='round'
+                                        d='M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3'
+                                    />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className='card-hover__extra'>
+                            <h5>
+                                Book <span>now</span> and get <span>{trip.discount}</span> discount!
+                            </h5>
+                        </div>
+                        <img src={trip.image} alt={trip.title} />
                     </div>
-                    <div className="card-hover__extra">
-                        <h5>
-                            Book <span>now</span> and get <span>10%</span> discount!
-                        </h5>
-                    </div>
-                    <img
-                        src={img}
-                        alt="Stone Town"
-                    />
                 </div>
-            </div>
-
-            <div className='trips'>
-                <div className="card-hover">
-                    <div className="card-hover__content">
-                        <h4 className="card-hover__title">
-                            Dhow & Snorkeling  <span>Safari</span> Blue
-                        </h4>
-                        <p className="card-hover__text">
-                            Experience the authentic and unrivaled Safari Blue - a full-day excursion aboard traditional, locally-crafted sailing dhows.
-                        </p>
-                        <a href="/learn-more" className="card-hover__link" aria-label="Learn more about Stone Town Heritage Walk">
-                            <span>Learn More</span>
-                            <svg
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                aria-label="External Link"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                                />
-                            </svg>
-                        </a>
-                    </div>
-                    <div className="card-hover__extra">
-                        <h5>
-                            Book <span>now</span> and get <span>10%</span> discount!
-                        </h5>
-                    </div>
-                    <img
-                        src={img2}
-                        alt="Stone Town"
-                    />
-                </div>
-            </div>
-
-            <div className='trips'>
-                <div className="card-hover">
-                    <div className="card-hover__content">
-                        <h4 className="card-hover__title">
-                            Zanzibar Spice & <span>Culture</span> Tour
-                        </h4>
-                        <p className="card-hover__text">
-                            Embark on a half-day journey through Central Zanzibar, exploring the rich history shaped by cloves, nutmeg, cinnamon, and pepper.
-                        </p>
-                        <a href="/learn-more" className="card-hover__link" aria-label="Learn more about Stone Town Heritage Walk">
-                            <span>Learn More</span>
-                            <svg
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                aria-label="External Link"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                                />
-                            </svg>
-                        </a>
-                    </div>
-                    <div className="card-hover__extra">
-                        <h5>
-                            Book <span>now</span> and get <span>10%</span> discount!
-                        </h5>
-                    </div>
-                    <img
-                        src={img3}
-                        alt="Stone Town"
-                    />
-                </div>
-            </div>
-
+            ))}
         </div>
     );
-}
+};
 
-export default Excursions
+export default Excursions;
