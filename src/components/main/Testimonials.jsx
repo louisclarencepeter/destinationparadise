1e2a60ebdd58aeaf7b50d353b24788b4adc739c3
Louis Peter <louisclarencepeters@gmail.com>
2023-12-10 10:58:08 +0100

    added the fonts and colors for the specific components

--- src/components/main/Testimonials.jsx
+++ src/components/main/Testimonials.jsx
@@ -49,15 +49,15 @@ const Testimonials = () => {
         />
         <Testimonial 
           imgSrc={img2} 
-          name="Isa Jua" 
+          name="Arturo" 
           review="&ldquo;This trip was amazing our tour guide was knowledgeable, upbeat and friendly .. the dowe experience was amazing and the lagoon was a site to see. I was afraid to snorkel but my tour guide got in with me and made me feel safe. The food they provided was very delicious I did not expect a buffet style meal on the beach ! This is what added to the experience! Will definitely recommend.&quot;"
           starCount={4} 
         />
         <Testimonial 
           imgSrc={img3} 
-          name="Isa Jua" 
+          name="Coleman" 
           review="&ldquo;We had a great time and really enjoyed smelling all the spices and learning about their uses, beyond cooking. Louis was very easy to communicate with and actually went above and beyond to help us book and brainstorm to resolve a couple of challenges that we had.&quot;"
-          starCount={3} 
+          starCount={5} 
         />
       </Slide>
       <button>Book Now</button>
