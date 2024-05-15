23005c3d863051697afb25c43f0ea947782cac39
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-05-15 05:25:26 +0200

    refactored the image component

--- src/components/home/Images.jsx
+++ src/components/home/Images.jsx
@@ -1,58 +1,51 @@
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+// Images.jsx
 import { useState, useEffect } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import "./Images.scss";
 
 const Images = () => {
-  const [imagesLoaded, setImagesLoaded] = useState(false);
   const [galleryImages, setGalleryImages] = useState([]);
 
   useEffect(() => {
-    const loadImages = () => {
+    const loadImages = async () => {
       const importedImages = [];
-  
+
       for (let i = 1; i <= 6; i++) {
-        // Direct URL path from the public folder
         const imagePath = `/gallery/${i}.jpg`;
-  
         importedImages.push({
           id: i,
           src: imagePath,
           alt: `Image ${i}`,
         });
       }
-  
-      // Set the state of the component
-      setImagesLoaded(true);
+
       setGalleryImages(importedImages);
     };
-  
+
     loadImages();
   }, []);
-  
+
   return (
-    <div className="gallery-container">
-      <h2 className="reveal">Gallery</h2>
-      {imagesLoaded && (
-        <ResponsiveMasonry
-          className="gallery-masonry"
-          columnsCountBreakPoints={{
-            350: 1,
-            750: 2,
-            900: 3,
-          }}
-        >
-          <Masonry className="gallery-images-container">
-            {galleryImages.map((image) => (
-              <img
-                className="gallery-image reveal"
-                key={image.id}
-                src={image.src}
-                alt={image.alt}
-              />
-            ))}
-          </Masonry>
-        </ResponsiveMasonry>
-      )}
+    <div className="gallery">
+      <h2 className="gallery__title">Gallery</h2>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{
+          350: 1,
+          750: 2,
+          900: 3,
+        }}
+      >
+        <Masonry>
+          {galleryImages.map((image) => (
+            <img
+              key={image.id}
+              src={image.src}
+              alt={image.alt}
+              className="gallery__image"
+            />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
     </div>
   );
 };
