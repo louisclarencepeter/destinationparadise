59e37b8d1bd8d2f76bf3bab9d335bc63b4a2af27
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-05-15 05:13:58 +0200

    refactored the hero component

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,23 +1,26 @@
+// Hero.jsx
 import './Hero.scss';
 import backgroundVideo from '../../assets/videos/background.mp4';
 
 function Hero() {
-    return (
-        <div className='hero-container'>
-            <video autoPlay loop muted className='video-background'>
-                <source src={backgroundVideo} type="video/mp4" />
-            </video>
-    
-                <div className='name-motto'>
-                    <h1>Destination Paradise</h1>
-                    <h3>Explore the beauty of Zanzibar</h3>
-                </div>
-
-                <p className='fade-in'>
-                    Welcome to Destination Paradise, your gateway to the enchanting Zanzibar Island! Imagine a place where each day is an adventure, and every horizon promises new discoveries.
-                </p>
+  return (
+    <div className="hero">
+      <div className="hero__background">
+        <video autoPlay loop muted className="hero__video">
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+      </div>
+      <div className="hero__content">
+        <div className="hero__heading">
+          <h1>Destination Paradise</h1>
+          <h3>Explore the beauty of Zanzibar</h3>
         </div>
-    );
+        <p className="hero__description">
+          Welcome to Destination Paradise, your gateway to the enchanting Zanzibar Island! Imagine a place where each day is an adventure, and every horizon promises new discoveries.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
