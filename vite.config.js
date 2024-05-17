0d384871fb32dc29edfe6223fe82321ec7945810
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-05-17 04:45:09 +0200

    added https force rule

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://localhost:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
