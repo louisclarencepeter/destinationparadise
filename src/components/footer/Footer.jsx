28994e00c28668efb69fa5cd864ca4ad54951d52
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-05-25 11:15:13 +0200

    added the appropiate links to the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,25 +18,20 @@ function Footer() {
         <div className="footer__contact">
           <p>info@yournexttriptoparadise.com</p>
           <div className="footer__social">
-            <a href="https://www.facebook.com/yourbusiness" target="_blank" rel="noopener noreferrer" aria-label="Visit our Facebook page">
+            <a href="https://www.facebook.com/yournexttriptoparadise/" target="_blank" rel="noopener noreferrer" aria-label="Visit our Facebook page">
               <i className="fab fa-facebook" aria-hidden="true"></i>
-              <span className="sr-only">Facebook</span>
             </a>
-            <a href="https://www.instagram.com/yourbusiness" target="_blank" rel="noopener noreferrer" aria-label="Visit our Instagram page">
+            <a href="https://www.instagram.com/yournexttriptoparadise/" target="_blank" rel="noopener noreferrer" aria-label="Visit our Instagram page">
               <i className="fab fa-instagram" aria-hidden="true"></i>
-              <span className="sr-only">Instagram</span>
             </a>
-            <a href="https://www.twitter.com/yourbusiness" target="_blank" rel="noopener noreferrer" aria-label="Visit our Twitter page">
-              <i className="fab fa-twitter" aria-hidden="true"></i>
-              <span className="sr-only">Twitter</span>
-            </a>
-            <a href="https://www.youtube.com/yourbusiness" target="_blank" rel="noopener noreferrer" aria-label="Visit our YouTube channel">
+            <a href="https://www.youtube.com/@destinationparadisezanzibar" target="_blank" rel="noopener noreferrer" aria-label="Visit our YouTube channel">
               <i className="fab fa-youtube" aria-hidden="true"></i>
-              <span className="sr-only">YouTube</span>
+            </a>
+            <a href="https://twitter.com/destinationpar5" target="_blank" rel="noopener noreferrer" aria-label="Visit our Twitter page">
+              <i className="fab fa-twitter" aria-hidden="true"></i>
             </a>
           </div>
         </div>
-        <p className="footer__copyright">© 2024 Destination Paradise. All rights reserved.</p>
       </div>
     </footer>
   );
