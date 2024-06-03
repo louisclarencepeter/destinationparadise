8ac46e8b26756f260f376936c767c6e31718c72c
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2024-06-03 16:51:33 +0200

    worked on the style of the chatbot on mobile devices

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     port: 3002,
     open: true,
+    host: '0.0.0.0', // This allows access from your local network
     proxy: {
       "/api": {
         target: "https://localhost:8000",
