142ab83f89bffd01e44ebfa2884f676f76713675
Louis Peter <louisclarencepeters@gmail.com>
2024-06-13 10:43:05 +0200

    Implement Netlify Forms for booking requests and integrate Zapier for email notifications

--- src/components/store/booking/BookingForm.jsx
+++ src/components/store/booking/BookingForm.jsx
@@ -15,48 +15,21 @@ const BookingForm = ({ tours, locations }) => {
     message: '',
   });
 
-  const [formStatus, setFormStatus] = useState('');
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch('/.netlify/functions/booking-request', {
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.ok) {
-        setFormStatus('Booking request submitted successfully.');
-        setFormData({
-          name: '',
-          email: '',
-          phone: '',
-          date: '',
-          tour: '',
-          location: '',
-          message: '',
-        });
-      } else {
-        setFormStatus('Failed to submit booking request.');
-      }
-    } catch (error) {
-      console.error('Error submitting form:', error);
-      setFormStatus('Error submitting booking request.');
-    }
-  };
-
   return (
-    <form onSubmit={handleSubmit} className="booking-form" name="booking-request" method="POST" data-netlify="true">
+    <form 
+      name="booking-request" 
+      method="POST" 
+      data-netlify="true" 
+      data-netlify-honeypot="bot-field" 
+      className="booking-form"
+    >
       <input type="hidden" name="form-name" value="booking-request" />
+      <input type="hidden" name="bot-field" />
       <FormInput label="Name:" id="name" name="name" value={formData.name} onChange={handleChange} required />
       <FormInput label="Email:" id="email" name="email" value={formData.email} onChange={handleChange} required />
       <FormInput label="Phone:" id="phone" name="phone" value={formData.phone} onChange={handleChange} required />
@@ -65,7 +38,6 @@ const BookingForm = ({ tours, locations }) => {
       <FormSelect label="Pick Up Location (Part of the Island):" id="location" name="location" value={formData.location} options={locations} onChange={handleChange} required />
       <FormTextarea label="Message:" id="message" name="message" value={formData.message} onChange={handleChange} required />
       <button type="submit" className="submit-button">Submit Booking Request</button>
-      {formStatus && <p>{formStatus}</p>}
     </form>
   );
 };
