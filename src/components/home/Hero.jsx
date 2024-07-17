7a0a60814595f1dfc5b043cec09b860619ba78d5
Louis Peter <louisclarencepeters@gmail.com>
2024-07-17 13:24:03 +0200

    Defer video loading with placeholder image to optimize LCP in Hero component.

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,22 +1,49 @@
 import './Hero.scss';
 import backgroundVideo from '../../assets/videos/background.mp4';
+import placeholderImage from '../../assets/images/boat.jpg';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const Hero = () => {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+
+  useEffect(() => {
+    const handleVideoLoad = () => {
+      setVideoLoaded(true);
+    };
+
+    const videoElement = document.querySelector('.hero__video');
+    if (videoElement) {
+      videoElement.addEventListener('loadeddata', handleVideoLoad);
+    }
+
+    return () => {
+      if (videoElement) {
+        videoElement.removeEventListener('loadeddata', handleVideoLoad);
+      }
+    };
+  }, []);
+
   return (
     <section className="hero">
-      <BackgroundVideo />
+      <BackgroundVideo videoLoaded={videoLoaded} />
       <HeroContent />
     </section>
   );
 };
 
-const BackgroundVideo = () => (
+const BackgroundVideo = ({ videoLoaded }) => (
   <div className="hero__background">
-    <video autoPlay loop muted playsInline className="hero__video">
+    {!videoLoaded && <img src={placeholderImage} alt="Placeholder" className="hero__placeholder" />}
+    <video autoPlay loop muted playsInline className={`hero__video ${videoLoaded ? 'visible' : 'hidden'}`}>
       <source src={backgroundVideo} type="video/mp4" />
     </video>
   </div>
 );
+
+BackgroundVideo.propTypes = {
+  videoLoaded: PropTypes.bool.isRequired,
+};
 
 const HeroContent = () => (
   <div className="hero__content">
@@ -28,7 +55,7 @@ const HeroContent = () => (
 const HeroHeading = () => (
   <div className="hero__heading">
     <h1>Destination Paradise</h1>
-    <h3><i>your next trip to Paradise..</i></h3>
+    <h3><i>Your next trip to Paradise..</i></h3>
   </div>
 );
 
