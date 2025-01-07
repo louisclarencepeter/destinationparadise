ea3df372d41e6c7e52f609eb036f966239367972
Louis Peter <louisclarencepeters@gmail.com>
2025-01-07 08:21:02 +0100

    feat: add tour descriptions to card with text truncation

--- src/components/excursions/TourCard.jsx
+++ src/components/excursions/TourCard.jsx
@@ -1,26 +1,20 @@
-// TourCard.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 import "./TourCard.scss";
 
 export default function TourCard({ tour }) {
-  // Destructure imageKey (instead of image)
-  const { title, duration, imageKey, price, currency = "$" } = tour;
+  const { title, duration, imageKey, price, description, currency = "$", id } = tour;
 
   return (
-    <Link to={`/excursions/${tour.id}`} style={{ textDecoration: "none" }}>
+    <Link to={`/excursions/${id}`} style={{ textDecoration: "none" }}>
       <div className="tour-card">
-        <div className="tour-image">
-          {/* Use imageKey here */}
+        <div className="tour-image" data-price={price}>
           <img src={imageKey} alt={title} />
         </div>
         <div className="tour-info">
           <p className="tour-duration">{duration}</p>
           <h3 className="tour-title">{title}</h3>
-          <p className="tour-price">
-            From {currency}
-            {price} / person
-          </p>
+          {description && <p className="tour-description">{description}</p>}
         </div>
       </div>
     </Link>
