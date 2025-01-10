d4646a37bf2b570a90dec66e59a33d88517aa690
Louis Peter <louisclarencepeters@gmail.com>
2025-01-10 06:20:52 +0100

    "Fix: Resolved Sass deprecation warnings by wrapping declarations in `& {}`."

---
+++ src/components/chatbot/TypingIndicator.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TypingIndicator.scss';
+
+const TypingIndicator = () => {
+  return (
+    <div className="typing-indicator">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+    </div>
+  );
+};
+
+export default TypingIndicator;
