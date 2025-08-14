fa560102eaa81d93543b288cd699560a4756a9eb
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2025-08-14 05:28:51 +0200

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,42 @@
 // Footer.jsx
+import { memo, useMemo } from 'react';
 import "./Footer.scss";
 import NewsletterForm from "./components/NewsletterForm";
 import ContactInfo from "./components/ContactInfo";
 import SocialLinks from "./components/SocialLinks";
 import LegalSection from "./components/LegalSection";
 
-function Footer() {
+const Footer = memo(function Footer() {
+  const currentYear = useMemo(() => new Date().getFullYear(), []);
+  
   return (
-    <footer className="footer">
+    <footer className="footer" role="contentinfo" aria-label="Site footer">
       <div className="footer__container">
-        <h3 className="footer__title">Subscribe to our newsletter! 🚀💬💌</h3>
-        <NewsletterForm />
-        <ContactInfo />
-        <SocialLinks />
-        <LegalSection />
-        <div className="footer__copyright">
-          <p>&copy; {new Date().getFullYear()} Destination Paradise. All rights reserved.</p>
+        <div className="footer__content">
+          <div className="footer__newsletter">
+            <h3 className="footer__title" id="newsletter-heading">
+              Subscribe to our newsletter! 🚀💬💌
+            </h3>
+            <NewsletterForm ariaLabelledBy="newsletter-heading" />
+          </div>
+          
+          <div className="footer__sections">
+            <ContactInfo />
+            <SocialLinks />
+            <LegalSection />
+          </div>
+        </div>
+        
+        <div className="footer__divider" role="separator" aria-hidden="true"></div>
+        
+        <div className="footer__bottom">
+          <div className="footer__copyright">
+            <p>&copy; {currentYear} Destination Paradise. All rights reserved.</p>
+          </div>
         </div>
       </div>
     </footer>
   );
-}
+});
 
 export default Footer;
