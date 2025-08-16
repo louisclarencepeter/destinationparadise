c08d3928e74fd4121560d7e2a7cbc9a706e36006
Louis Peter <109584572+louisclarencepeter@users.noreply.github.com>
2025-08-16 13:05:56 +0200

    Update HamburgerToggle.jsx

--- src/components/header/components/HamburgerToggle.jsx
+++ src/components/header/components/HamburgerToggle.jsx
@@ -1,26 +1,22 @@
+// HamburgerToggle.jsx
 import PropTypes from "prop-types";
 import "./HamburgerToggle.scss";
 
-const HamburgerToggle = ({ isOpen, toggleMenu }) => (
-  <div
-    className="menu__toggle-container"
-    role="button"
-    aria-expanded={isOpen}
-    aria-controls="menu__box"
-    tabIndex={0}
-    onClick={toggleMenu}
-    onKeyDown={(e) => (e.key === "Enter" || e.key === " ") && toggleMenu()}
-  >
-    <input id="menu__toggle" type="checkbox" checked={isOpen} onChange={toggleMenu} />
-    <label
-      className="menu__btn"
-      htmlFor="menu__toggle"
+const HamburgerToggle = ({ isOpen, toggleMenu }) => {
+  return (
+    <button
+      type="button"
+      className={`hamburger-toggle ${isOpen ? "is-open" : ""}`}
+      aria-expanded={isOpen}
+      aria-controls="menu__box"
       aria-label={isOpen ? "Close menu" : "Open menu"}
+      onClick={toggleMenu}
     >
-      <span></span>
-    </label>
-  </div>
-);
+      {/* The three bars */}
+      <span className="bars" aria-hidden="true" />
+    </button>
+  );
+};
 
 HamburgerToggle.propTypes = {
   isOpen: PropTypes.bool.isRequired,
