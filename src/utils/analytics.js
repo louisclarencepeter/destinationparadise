f1f08004498c2aff6a2e951f2c007d4dd5609ea4
Louis Peter <louisclarencepeters@gmail.com>
2026-07-10 23:28:42 +0200

    Fix Google Analytics tag routing

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,3 +1,4 @@
+const GOOGLE_TAG_ID = 'GT-WVRZ4PC6';
 const GOOGLE_ANALYTICS_ID = 'G-44V46CDF6Y';
 const CONSENT_STORAGE_KEY = 'dp_cookie_consent_v1';
 
@@ -26,15 +27,15 @@ export function loadGoogleAnalytics() {
     dataLayer.push(arguments);
   };
 
-  if (!document.querySelector(`script[src*="googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}"]`)) {
+  if (!document.querySelector(`script[src*="googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}"]`)) {
     const script = document.createElement('script');
     script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`;
     document.head.appendChild(script);
   }
 
   window.gtag('js', new Date());
-  window.gtag('config', GOOGLE_ANALYTICS_ID, { send_page_view: false });
+  window.gtag('config', GOOGLE_TAG_ID, { send_page_view: false });
   analyticsReady = true;
   return true;
 }
@@ -78,6 +79,7 @@ export function trackPageView(path) {
   if (typeof window.gtag !== 'function') return;
 
   window.gtag('event', 'page_view', {
+    send_to: GOOGLE_ANALYTICS_ID,
     page_path: path,
     page_location: window.location.href,
     page_title: document.title,
